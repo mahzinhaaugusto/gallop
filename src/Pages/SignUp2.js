5f4d5ae8914e960ee19b720b4972261b37411b94
Madhu Nyoupane <nyoupanemadhu07@gmail.com>
2023-02-26 14:47:31 -0800

    feat: add camera feature

--- src/Pages/SignUp2.js
+++ src/Pages/SignUp2.js
@@ -1,17 +1,54 @@
 import { useLocation } from "react-router-dom";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Axios from "axios";
 import { useNavigate } from "react-router-dom";
 import ellipse from "../icons/Ellipse.svg";
 import ellipse2 from "../icons/Ellipse2.svg";
 import camera from "../icons/Camera.svg";
+
 export function SignUp2() {
   const location = useLocation();
   const [Address, setAddress] = useState("");
   const [Website, setWebsite] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
+  const [userPhoto, setUserPhoto] = useState("");
 
   let navigate = useNavigate();
+
+  const videoRef = useRef(null);
+  const photoRef = useRef(null);
+  const getVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: { width: 1920, height: 1080 },
+      })
+      .then((stream) => {
+        let video = videoRef.current;
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  const takePhoto = () => {
+    const width = 414;
+    const height = width / (16 / 9);
+    let video = videoRef.current;
+    let photo = photoRef.current;
+
+    photo.width = width;
+    photo.height = height;
+
+    let ctx = photo.getContext("2d");
+
+    ctx.drawImage(video, 0, 0, width, height);
+    console.log(photo.toDataURL());
+    document.getElementById("cameraCont").style.display = "none";
+    document.getElementById("profilePhoto").src = photo.toDataURL();
+    setUserPhoto(photo.toDataURL());
+  };
 
   const submitClicked = () => {
     Axios.post("http://localhost:3002/api/insert", {
@@ -25,6 +62,12 @@ export function SignUp2() {
     });
     alert("data written successfully");
     navigate("/");
+  };
+
+  const profilePicture = () => {
+    // cameraOn = true;
+    document.getElementById("cameraCont").style.display = "block";
+    getVideo();
   };
 
   return (
@@ -52,10 +95,23 @@ export function SignUp2() {
         </div>
 
         <div className="signUp2Cont_form">
-          <div className="signUp2Cont_form_image"></div>
-          <div className="signUp2Cont_form_camera">
-            <img src={camera} alt="not found" />
+          <div className="signUp2Cont_form_image">
+            <img
+              id="profilePhoto"
+              src={camera}
+              alt="not found"
+              onClick={profilePicture}
+            />
           </div>
+
+          <div className="camera_Cont" id="cameraCont">
+            <video ref={videoRef}></video>
+            <button onClick={takePhoto}>Click</button>
+            <div className="result">
+              <canvas ref={photoRef}></canvas>
+            </div>
+          </div>
+
           <label className="signUp2Cont_form_phoneNumberLabel require">
             PhoneNumber
           </label>
@@ -85,7 +141,6 @@ export function SignUp2() {
               setWebsite(e.target.value);
             }}
           ></input>
-
           <button onClick={submitClicked}>Create An Account</button>
           <button>Back</button>
           <p>{phoneNumber}</p>
