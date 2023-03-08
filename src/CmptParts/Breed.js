af65b3bbd731d9ef9b0eb8a22d31b69c7e48cd20
Marina Faria <marina.acfaria@gmail.com>
2023-03-07 18:29:26 -0800

    refactor: Different spaces between Madhu and I

--- src/CmptParts/Breed.js
+++ src/CmptParts/Breed.js
@@ -3,130 +3,129 @@ import { useState, useEffect, useRef } from "react";
 import breedList from "../breedList.json";
 
 export function Breed(props) {
-  const options = breedList.breedList;
+    const options = breedList.breedList;
 
-  return (
-    <div className="filter_cont_breed_exterior">
-      <label className="filter_cont_breed_label">
-        Breed <span className="filter_cont_breed_label_error">*</span>
-      </label>
-      <div className="filter_cont_breed">
-        <BreedDropdown isSearchable placeholder="Select" options={options} />
-      </div>
-    </div>
-  );
+    return (
+        <div className="filter_cont_breed_exterior">
+            <label className="filter_cont_breed_label">
+                Breed <span className="filter_cont_breed_label_error">*</span>
+            </label>
+            <div className="filter_cont_breed">
+                <BreedDropdown isSearchable placeholder="Select" options={options} />
+            </div>
+        </div>
+    );
 }
 
 function BreedDropdown({ placeholder, options, isSearchable }) {
-  const [showOptions, setShowOptions] = useState(false);
+    const [showOptions, setShowOptions] = useState(false);
 
-  const [selectedValue, setSelectedValue] = useState(null);
+    const [selectedValue, setSelectedValue] = useState(null);
 
-  const inputRef = useRef();
+    const inputRef = useRef();
 
-  const [searchValue, setSearchValue] = useState("");
+    const [searchValue, setSearchValue] = useState("");
 
-  const searchRef = useRef();
+    const searchRef = useRef();
 
-  // Search
-  useEffect(() => {
-    setSearchValue("");
-    if (showOptions && searchRef.current) {
-      searchRef.current.focus();
-    }
-  }, [showOptions]);
+    // Search
+    useEffect(() => {
+        setSearchValue("");
+        if (showOptions && searchRef.current) {
+            searchRef.current.focus();
+        }
+    }, [showOptions]);
 
-  const onSearch = (event) => {
-    setSearchValue(event.target.value);
-  };
-
-  const getOptions = () => {
-    if (!searchValue) {
-      return options;
-    }
-    return options.filter(
-      (option) =>
-        option.label.toLowerCase().indexOf(searchValue.toLowerCase()) >= 0
-    );
-  };
-
-  useEffect(() => {
-    const handler = (event) => {
-      if (inputRef.current && !inputRef.current.contains(event.target)) {
-        setShowOptions(false);
-      }
+    const onSearch = (event) => {
+        setSearchValue(event.target.value);
     };
 
-    window.addEventListener("click", handler);
-
-    return () => {
-      window.removeEventListener("click", handler);
+    const getOptions = () => {
+        if (!searchValue) {
+            return options;
+        }
+        return options.filter(
+            (option) =>
+                option.label.toLowerCase().indexOf(searchValue.toLowerCase()) >= 0
+        );
     };
-  });
 
-  const handleInputClick = (event) => {
-    // event.stopPropagation();
-    setShowOptions(!showOptions);
-  };
+    useEffect(() => {
+        const handler = (event) => {
+            if (inputRef.current && !inputRef.current.contains(event.target)) {
+                setShowOptions(false);
+            }
+        };
 
-  const getBreedSelection = () => {
-    if (selectedValue) {
-      return selectedValue.label;
-    }
-    return placeholder;
-  };
+        window.addEventListener("click", handler);
 
-  const onItemClick = (option) => {
-    setSelectedValue(option);
-    console.log(selectedValue.value);
-    //props.onClick(option);
-  };
+        return () => {
+            window.removeEventListener("click", handler);
+        };
+    });
 
-  const isSelected = (option) => {
-    if (!selectedValue) {
-      return false;
-    }
+    const handleInputClick = (event) => {
+        // event.stopPropagation();
+        setShowOptions(!showOptions);
+    };
 
-    return selectedValue.value === option.value;
-  };
+    const getBreedSelection = () => {
+        if (selectedValue) {
+            return selectedValue.label;
+        }
+        return placeholder;
+    };
 
-  return (
-    <div className="filter_cont_breed_dropdown">
-      <div
-        ref={inputRef}
-        onClick={handleInputClick}
-        className="filter_cont_breed_dropdown_selector"
-      >
-        <div className="filter_cont_breed_selector_selection">
-          {getBreedSelection()}
-        </div>
-        <DropdownIcon />
-      </div>
-      {showOptions && (
-        <div className="filter_cont_breed_dropdown_options">
-          {isSearchable && (
-            <div className="filter_cont_breed_dropdown_search">
-              <input
-                type="text"
-                onChange={onSearch}
-                value={searchValue}
-                ref={searchRef}
-              />
-            </div>
-          )}
-          {getOptions().map((option) => (
+    const onItemClick = (option) => {
+        setSelectedValue(option);
+        console.log(option.value);
+        //props.onClick(option);
+    };
+
+    const isSelected = (option) => {
+        if (!selectedValue) {
+            return false;
+        }
+
+        return selectedValue.value === option.value;
+    };
+
+    return (
+        <div className="filter_cont_breed_dropdown">
             <div
-              onClick={() => onItemClick(option)}
-              key={option.value}
-              className={`filter_cont_breed_dropdown_options_singleOption ${
-                isSelected(option) && "selected"
-              }`}
+                ref={inputRef}
+                onClick={handleInputClick}
+                className="filter_cont_breed_dropdown_selector"
             >
-              {option.label}
+                <div className="filter_cont_breed_selector_selection">
+                    {getBreedSelection()}
+                </div>
+                <DropdownIcon />
             </div>
-          ))}
+            {showOptions && (
+                <div className="filter_cont_breed_dropdown_options">
+                    {isSearchable && (
+                        <div className="filter_cont_breed_dropdown_search">
+                            <input
+                                type="text"
+                                onChange={onSearch}
+                                value={searchValue}
+                                ref={searchRef}
+                            />
+                        </div>
+                    )}
+                    {getOptions().map((option) => (
+                        <div
+                            onClick={() => onItemClick(option)}
+                            key={option.value}
+                            className={`filter_cont_breed_dropdown_options_singleOption ${isSelected(option) && "selected"
+                                }`}
+                        >
+                            {option.label}
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
