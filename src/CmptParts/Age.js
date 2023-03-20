7db961fcea4268560df17f19d615ce75c6ea53c8
Marina Faria <marina.acfaria@gmail.com>
2023-03-20 00:42:42 -0700

    refactor: Preventing from user to use scroll
    When using mouse scroller the input number would automatically change.

--- src/CmptParts/Age.js
+++ src/CmptParts/Age.js
@@ -3,8 +3,8 @@ export function Age() {
         <div className="filter_cont_age_exterior">
             <label className="filter_cont_age_label">Age</label>
             <div className="filter_cont_age">
-                <input id="minAge" type="number" min="1" max="35" placeholder="min" />
-                <input id="maxAge" type="number" min="1" max="35" placeholder="max" />
+                <input id="minAge" type="number" min="1" max="35" placeholder="min" onWheel={(e) => e.target.blur()} />
+                <input id="maxAge" type="number" min="1" max="35" placeholder="max" onWheel={(e) => e.target.blur()} />
             </div>
         </div>
     )
