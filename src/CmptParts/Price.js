0f9fde585b766db56660b41d7f41a002dc754188
edenLangara <soh25@mylangara.ca>
2023-03-23 11:32:33 -0700

    Fix filter min and max

--- src/CmptParts/Price.js
+++ src/CmptParts/Price.js
@@ -1,10 +1,24 @@
 export function Price() {
     return (
         <div className="filter_cont_price_exterior">
-            <label className="filter_cont_price_label">Price</label>
+            <label className="filter_cont_price_label">Price (Dollars)</label>
             <div className="filter_cont_price">
-                <input id="minPrice" type="number" min="0" placeholder="min" onWheel={(e) => e.target.blur()} />
-                <input id="maxPrice" type="number" min="1" placeholder="max" onWheel={(e) => e.target.blur()} />
+                <input id="minPrice" type="number" min="1" placeholder="min" 
+                    onWheel={(e) => e.target.blur()}
+                    onChange={()=> {
+                        if(document.getElementById("minPrice").value < 0) {
+                            document.getElementById("minPrice").value = 1;
+                        }
+                    }} 
+                />
+                <input id="maxPrice" type="number" min="1" placeholder="max"
+                    onWheel={(e) => e.target.blur()}
+                    onChange={()=> {
+                        if(document.getElementById("maxPrice").value < document.getElementById("minPrice").value) {
+                            document.getElementById("maxPrice").value = Number(document.getElementById("minPrice").value)+1;
+                        }
+                    }} 
+                />
             </div>
         </div>
     )
