615eca115b40b63c9c10d04fb72f36c9f3a29240
Madhu Nyoupane <nyoupanemadhu07@gmail.com>
2023-03-24 19:34:37 -0700

    feat: implement function of filter

--- src/CmptParts/Gender.js
+++ src/CmptParts/Gender.js
@@ -1,31 +1,46 @@
 import { useState } from "react";
 
-export function Gender() {
-    const [checkedStallion, setCheckedStallion] = useState(false);
-    const [checkedMare, setCheckedMare] = useState(false);
+export function Gender(props) {
+  const [checkedStallion, setCheckedStallion] = useState(false);
+  const [checkedMare, setCheckedMare] = useState(false);
+  // console.log(props.handleMessage);
+  const handlerStallion = () => {
+    setCheckedStallion(!checkedStallion);
+    props.handleMessage("stallion");
+    // console.log("stallion");
+  };
+  const handlerMare = () => {
+    setCheckedMare(!checkedMare);
+    props.handleMessage("mare");
+    // console.log("mare");
+  };
 
-
-    const handlerStallion = () => {
-        setCheckedStallion(!checkedStallion);
-        console.log("stallion");
-    }
-    const handlerMare = () => {
-        setCheckedMare(!checkedMare);
-        console.log("mare");
-    }
-
-    return (
-        <div className="filter_cont_gender">
-            <div className="filter_cont_gender_stallion">
-                <label>
-                    <input name="genres" id="stallion" type="radio" checked={checkedStallion} onChange={handlerStallion} />
-                    Stallion</label>
-            </div>
-            <div className="filter_cont_gender_mare">
-                <label>
-                    <input name="genres" id="mare" type="radio" checked={checkedMare} onChange={handlerMare} />
-                    Mare</label>
-            </div>
-        </div>
-    )
+  return (
+    <div className="filter_cont_gender">
+      <div className="filter_cont_gender_stallion">
+        <label>
+          <input
+            name="genres"
+            id="stallion"
+            type="radio"
+            checked={checkedStallion}
+            onChange={handlerStallion}
+          />
+          Stallion
+        </label>
+      </div>
+      <div className="filter_cont_gender_mare">
+        <label>
+          <input
+            name="genres"
+            id="mare"
+            type="radio"
+            checked={checkedMare}
+            onChange={handlerMare}
+          />
+          Mare
+        </label>
+      </div>
+    </div>
+  );
 }
