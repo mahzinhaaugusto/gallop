5d443b7adac732c423ddec4138308fc2901f55ba
Marina Faria <marina.acfaria@gmail.com>
2023-03-29 01:41:29 -0700

    refactor: Adjustments on the charts

--- src/Pages/DataAnalytics.js
+++ src/Pages/DataAnalytics.js
@@ -1,6 +1,6 @@
 import { Footer } from "../Components/Footer";
 import { NavBar } from "../Components/NavBar";
-import BackButton from "../icons/BackButton.svg";
+// import BackButton from "../icons/BackButton.svg";
 import { BarChart, XAxis, YAxis, Tooltip, Bar, PieChart, Pie } from "recharts";
 import { groupBy } from "lodash";
 import { useState, useEffect } from "react";
@@ -9,9 +9,9 @@ import { API_ENDPOINT } from "../server";
 
 
 export function DataAnalytics() {
-    const goBack = () => {
-        console.log("working");
-    }
+    // const goBack = () => {
+    //     console.log("working");
+    // }
 
     const [allHorsesBreed, setHorsesBreed] = useState([]);
     const [allHorsesGender, setHorsesGender] = useState([]);
@@ -58,39 +58,41 @@ export function DataAnalytics() {
                 <h2 className="dataAnalytics_cont_master_title">Get to Know Our Horses!</h2>
                 <div className="dataAnalytics_cont_master_inner">
                     <div className="dataAnalytics_cont">
-                        <div className="dataAnalytics_cont_backBtn">
+                        {/* <div className="dataAnalytics_cont_backBtn">
                             <p className="addHorse_cont_backButton" onClick={goBack}>
                                 <img src={BackButton} height="30px" width="30px" alt="Go Back" />
                                 Back
                             </p>
-                        </div>
+                        </div> */}
                         <div className="dataAnalytics_cont_content">
                             <div className="dataAnalytics_cont_title">
                                 <h1 className="dataAnalytics_cont_title_text">Horses in Gallop</h1>
+                                <hr />
                             </div>
                             <div className="dataAnalytics_cont_mainContent">
                                 <div className="dataAnalytics_cont_mainContent_breed">
-                                    <h2 className="dataAnalytics_cont_mainContent_breed_title">All Around the World</h2>
+                                    <h3 className="dataAnalytics_cont_mainContent_breed_title">All Around the World</h3>
                                     <p className="dataAnalytics_cont_mainContent_breed_content">In Gallop, there are many different breeds of horses. Discover the top breeds of horses on Gallop and explore where they are originally from around the world.</p>
                                     <div className="dataAnalytics_cont_mainContent_breed_chart">
-                                        <BarChart width={1000} height={250} data={allHorsesBreed} margin={{
+                                        <BarChart className="barChart" width={1000} height={250} data={allHorsesBreed} margin={{
                                             top: 5,
                                             right: 30,
                                             left: 20,
                                             bottom: 5,
-                                        }} >
-                                            <XAxis dataKey="breed" interval={0} fontSize={11} />
+                                        }} domain={['auto', 'auto']} >
+                                            <XAxis dataKey="breed" interval={0} fontSize={10} />
                                             <YAxis />
                                             <Tooltip />
-                                            <Bar dataKey="quantity" fill="#75B367" />
+                                            <Bar dataKey="quantity" fill="#75B367" barSize={25} />
                                         </BarChart>
                                     </div>
                                 </div>
+                                <hr />
                                 <div className="dataAnalytics_cont_mainContent_gender">
-                                    <h2 className="dataAnalytics_cont_mainContent_gender_title">Gender</h2>
+                                    <h3 className="dataAnalytics_cont_mainContent_gender_title">Gender</h3>
                                     <p className="dataAnalytics_cont_mainContent_gender_content">Those are the Stallions and Mare on Gallop. Easily track the number of stallions and mares by viewing the percentage of males and females listed on our plataform.</p>
                                     <div className="dataAnalytics_cont_mainContent_gender_chart">
-                                        <PieChart width={730} height={250}>
+                                        <PieChart className="pieChart" width={250} height={250}>
                                             <Tooltip />
                                             <Pie data={allHorsesGender} dataKey="quantity" nameKey="gender" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#5A5A5F" label />
                                         </PieChart>
@@ -105,3 +107,15 @@ export function DataAnalytics() {
         </div >
     )
 }
+
+// function CustomizedAxisTick() {
+//     const { x, y, payload } = this.props;
+
+//     return (
+//         <g transform={`translate(${x},${y})`}>
+//             <text x={0} y={0} dy={16} textAnchor="end" fill="#75B367" transform="rotate(-35)">
+//                 {payload.value}
+//             </text>
+//         </g>
+//     );
+// }
